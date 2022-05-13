eac527936eebb16e04811fb686e0499e6a3d8846
Diego I <diego@sourcecode.team>
2022-05-13 17:36:34 +0200

    start screen

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <main class="flex flex-col text-center h-screen justify-between">
+    <h1 class="text-white text-2xl">Outer Space Z</h1>
+
+    <span class="h7 text-white flex p-5 mx-5 text-center justify-center">
+      New adventure
+    </span>
+    <span class="h20"></span>
+  </main>
+</template>
+<style scoped>
+span {
+  background: url('@/assets/button-bg.png') 50% 50% no-repeat;
+  background-size:contain;
+}
+</style>
