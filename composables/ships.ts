16fce67ff22e83ac22a4d5e99bc4a5ac81fe2b0f
Diego Imbriani <diego@sourcecode.team>
2024-03-13 23:31:21 +0100

    wip

---
+++ composables/ships.ts
@@ -0,0 +1,157 @@
+export const useShips = () => {
+  return ref([
+    {
+      "id": "1",
+      "name": "X11",
+      "agi": "10",
+      "hull": "4",
+      "cargo": "24",
+      "weapon_slots": "1",
+      "shield_slots": "1"
+    },
+    {
+      "id": "2",
+      "name": "B6",
+      "agi": "6",
+      "hull": "8",
+      "cargo": "24",
+      "weapon_slots": "1",
+      "shield_slots": "1"
+    },
+    {
+      "id": "3",
+      "name": "ERP-1",
+      "agi": "8",
+      "hull": "6",
+      "cargo": "24",
+      "weapon_slots": "1",
+      "shield_slots": "1"
+    },
+    {
+      "id": "4",
+      "name": "FL-Y",
+      "agi": "22",
+      "hull": "18",
+      "cargo": "32",
+      "weapon_slots": "2",
+      "shield_slots": "2"
+    },
+    {
+      "id": "5",
+      "name": "MR-22",
+      "agi": "28",
+      "hull": "18",
+      "cargo": "26",
+      "weapon_slots": "2",
+      "shield_slots": "3"
+    },
+    {
+      "id": "6",
+      "name": "XX",
+      "agi": "32",
+      "hull": "16",
+      "cargo": "24",
+      "weapon_slots": "3",
+      "shield_slots": "2"
+    },
+    {
+      "id": "7",
+      "name": "MANTIS",
+      "agi": "42",
+      "hull": "22",
+      "cargo": "34",
+      "weapon_slots": "3",
+      "shield_slots": "2"
+    },
+    {
+      "id": "8",
+      "name": "H-2",
+      "agi": "14",
+      "hull": "40",
+      "cargo": "44",
+      "weapon_slots": "3",
+      "shield_slots": "3"
+    },
+    {
+      "id": "9",
+      "name": "OCT-8",
+      "agi": "22",
+      "hull": "32",
+      "cargo": "44",
+      "weapon_slots": "4",
+      "shield_slots": "2"
+    },
+    {
+      "id": "10",
+      "name": "FF-Y",
+      "agi": "18",
+      "hull": "38",
+      "cargo": "50",
+      "weapon_slots": "3",
+      "shield_slots": "3"
+    },
+    {
+      "id": "11",
+      "name": "MBM-12",
+      "agi": "16",
+      "hull": "44",
+      "cargo": "46",
+      "weapon_slots": "2",
+      "shield_slots": "4"
+    },
+    {
+      "id": "12",
+      "name": "FSM-X",
+      "agi": "10",
+      "hull": "54",
+      "cargo": "66",
+      "weapon_slots": "3",
+      "shield_slots": "4"
+    },
+    {
+      "id": "13",
+      "name": "YTT",
+      "agi": "10",
+      "hull": "48",
+      "cargo": "72",
+      "weapon_slots": "2",
+      "shield_slots": "5"
+    },
+    {
+      "id": "14",
+      "name": "HAL-1",
+      "agi": "6",
+      "hull": "60",
+      "cargo": "74",
+      "weapon_slots": "1",
+      "shield_slots": "7"
+    },
+    {
+      "id": "15",
+      "name": "RN-77",
+      "agi": "12",
+      "hull": "66",
+      "cargo": "62",
+      "weapon_slots": "4",
+      "shield_slots": "4"
+    },
+    {
+      "id": "16",
+      "name": "BM64",
+      "agi": "8",
+      "hull": "78",
+      "cargo": "80",
+      "weapon_slots": "4",
+      "shield_slots": "5"
+    },
+    {
+      "id": "17",
+      "name": "DDM-2",
+      "agi": "12",
+      "hull": "64",
+      "cargo": "90",
+      "weapon_slots": "3",
+      "shield_slots": "6"
+    }
+  ])
+}
--- pages/setup/ship.vue
+++ pages/setup/ship.vue
@@ -10,25 +10,26 @@
 						<MazBtn color="transparent" @click="prev" noPadding>
 							<Icon name="tabler:square-rounded-chevrons-left-filled" size="40" />
 						</MazBtn>
-						<SetupShip :index="index" :locked="isLocked(index)" />
+						<SetupShip :index="index + 1" :locked="isLocked" />
 						<MazBtn color="transparent" @click="next" noPadding>
 							<Icon name="tabler:square-rounded-chevrons-right-filled" size="40" />
 						</MazBtn>
 					</UiRow>
 				</UiRow>
 			</div>
-			<UiRow class="border-t border-slate-800 border-b py-4 justify-around">
+			<h2>{{ ships[index].name }}</h2>
+			<UiRow class="border-t border-slate-800 border-b py-4 justify-around bg-black bg-opacity-15">
 				<UiCol class="gap-2">
 					<UiRow class="gap-2">
 						<img src="@/assets/ui/icon-agi.png" class="w-[20px]" />
-						<span class="text-xl">80</span>
+						<span class="text-xl">{{ agi }}</span>
 					</UiRow>
 					<h3 class="text-xs text-center text-cyan-300">Agility</h3>
 				</UiCol>
 				<UiCol class="gap-2">
 					<UiRow class="gap-2">
 						<img src="@/assets/ui/icon-hull.png" class="w-[20px]" />
-						<span class="text-xl">80</span>
+						<span class="text-xl">{{ hull }}</span>
 					</UiRow>
 					<h3 class="text-xs text-center text-cyan-300">Hull</h3>
 				</UiCol>
@@ -36,7 +37,7 @@
 				<UiCol class="gap-2">
 					<UiRow class="gap-2">
 						<img src="@/assets/ui/icon-cargo.png" class="w-[20px]" />
-						<span class="text-xl">80</span>
+						<span class="text-xl">{{ cargo }}</span>
 					</UiRow>
 					<h3 class="text-xs text-center text-cyan-300">Cargo</h3>
 				</UiCol>
@@ -47,7 +48,10 @@
 </template>
 <script setup>
 import { useStorage } from '@vueuse/core'
-const index = useStorage('ship', () => 1)
+
+const ships = useShips()
+
+const index = useStorage('ship', () => 0)
 
 onMounted(() => {
 	particlesJS.load('particles-js', '/stars1.json', function () {
@@ -55,20 +59,30 @@ onMounted(() => {
 	});
 })
 
-function isLocked(index) {
-	return index > 2
-}
-
-const count = 17
+const isLocked = computed(() => {
+	return index.value > 2
+})
 
 function next() {
-	index.value = (index.value + 1) % (count + 1) || 1
+	index.value = (index.value + 1) % ships.value.length
 }
 function prev() {
-	index.value = index.value - 1 || (count - 1)
+	index.value = (index.value + ships.value.length - 1) % ships.value.length
 }
 
 const nextPage = computed(() => {
-	return isLocked(index.value) ? null : '/setup/summary'
+	return isLocked.value ? null : '/setup/summary'
+})
+
+const agi = computed(() => {
+	return isLocked.value ? '?' : ships.value[index.value].agi
+})
+
+const hull = computed(() => {
+	return isLocked.value ? '?' : ships.value[index.value].hull
+})
+
+const cargo = computed(() => {
+	return isLocked.value ? '?' : ships.value[index.value].cargo
 })
 </script>
