c5b135dea0c541fc4b768f9262437d8a554420ef
Diego Imbriani <diego@sourcecode.team>
2024-03-17 13:08:20 +0100

    wip

--- pages/cockpit.vue
+++ pages/cockpit.vue
@@ -26,14 +26,7 @@
     </div>
 
     <!-- tabs -->
-    <div
-      class="w-full flex items-center justify-around absolute z-30 bottom-0 left-0 right-0 h-[60px] bg-black bg-opacity-90 border-t-2 border-white border-opacity-10">
-      <SvgoCockpit class="text-4xl text-white" />
-      <SvgoChart class="text-4xl text-white opacity-45" />
-      <SvgoShipyard class="text-4xl text-white opacity-45" />
-      <SvgoUser class="text-4xl text-white opacity-45" />
-      <SvgoOptions class="text-4xl text-white opacity-45" />
-    </div>
+    <Tabs active='cockpit' />
   </NuxtLayout>
 </template>
 
